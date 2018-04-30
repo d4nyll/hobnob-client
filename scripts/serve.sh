549d187eabc5d631e209e96a5e59de5a87b5ee6c
Daniel Li <dan@danyll.com>
2018-04-30 22:47:51 +0100

    Add API as part of serve script

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -4,6 +4,9 @@
 source <(dotenv-export | sed 's/\\n/\n/g')
 export NODE_ENV=test
 
+yarn run api:update
+yarn run api:install
+yarn run api:serve &
 yarn run build
 
 http-server dist/ -p $WEB_SERVER_PORT_TEST
